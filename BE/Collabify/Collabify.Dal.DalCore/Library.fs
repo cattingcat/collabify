71f4e71a237d61ebba1c23825596d4a00532e144
Evgeniy Kurpanov <jn.project0@gmail.com>
2018-11-02 23:27:30 +0300

    MongoDb FSHORP ITS ALIVE

--- BE/Collabify/Collabify.Dal.DalCore/Library.fs
+++
@@ -1,9 +0,0 @@
-namespace Collabify.Dal.DalCore
-
-module SomeProcess =
-    open MongoCrud
-    open MongoDB.Bson
-    
-    let task: TaskData = { Id = new BsonObjectId(ObjectId.GenerateNewId()); Title = "Titly"; Body = "SomeBody"; }
-    
-    MongoCrud.create task
--- BE/Collabify/Collabify.Dal.DalCore/SettingsUp.fs
+++ BE/Collabify/Collabify.Dal.DalCore/SettingsUp.fs
@@ -2,12 +2,13 @@ namespace Collabify.Dal.DalCore
 
 module MongoCrud =
 
+    open System.Threading.Tasks
     open MongoDB.Bson
     open MongoDB.Driver
     open MongoDB.Driver.Builders
     
     [<Literal>]
-    let ConnectionString = "mongodb://hippoproject:project0areowned@ds227469.mlab.com:27469/mognotestdb"
+    let ConnectionString = "mongodb://adolf:hitler1488@ds227469.mlab.com:27469/mognotestdb"
     
     [<Literal>]
     let DbName = "mognotestdb"
@@ -33,14 +34,50 @@ module MongoCrud =
         
     // Read 
     
-    // Read Based On Id 
-    let readOnId ( id : BsonObjectId ) = 
+    // Find by Id 
+    let findById ( id : BsonObjectId ) = 
         testCollection.Find( fun x -> x.Id = id ).ToEnumerable() 
     
-    // Read Based On Name 
-    let readOnName ( title : string ) = 
+    // Find by Name 
+    let findByName ( title : string ) = 
         testCollection.Find( fun x -> x.Title = title ).ToEnumerable() 
     
-    // Read Based On Name and Id
-    let readOnNameAndId ( id : BsonObjectId ) ( title : string ) =
-        testCollection.Find( fun x -> x.Id = id && x.Title = title ).ToEnumerable() 
+    // Find by Name and Id
+    let findByNameAndId ( id : BsonObjectId ) ( title : string ) =
+        testCollection.Find( fun x -> x.Id = id && x.Title = title ).ToEnumerable()
+        
+    // Update
+    
+    // Update one
+    let update ( td : TaskData) =
+        let filter = Builders<TaskData>.Filter.Eq((fun x -> x.Id), td.Id)
+        let updateDefinition = Builders<TaskData>.Update.Set((fun x -> x), td)
+        testCollection.UpdateOne(filter, updateDefinition)
+    
+    // Delete
+    
+    // Delete by id
+    let deleteById ( id : BsonObjectId) =
+        testCollection.DeleteOne(fun x -> x.Id = id)
+
+    // Delete all        
+    let deleteAll =
+        testCollection.DeleteMany(Builders.Filter.Empty)
+    
+    // some testing
+    let id = new BsonObjectId(ObjectId.GenerateNewId())
+    let task: TaskData = { Id = id; Title = "Title1"; Body = "SomeBody1"; }
+
+    [<EntryPoint>]
+    let main args =
+        printfn "creating"
+        create task
+        printfn "Title: %s, Body: %s" task.Title task.Body
+        
+        printfn "getting"
+        let task: TaskData = findById id |> Seq.head
+        printfn "Title: %s, Body: %s" task.Title task.Body
+        
+        printfn "findall"
+        
+        0
